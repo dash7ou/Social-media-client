67278a38f7a253e6945e3bad46adb3d5038ed62c
DasH <fxfddfno@gmail.com>
2020-05-23 16:25:24 +0300

    setup app router and show our nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router , Route, Switch , Redirect } from "react-router-dom";
+
+import Navbar from "./shared/components/Navbar"
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 import './App.css';
 
 const App = ()=>{
-  return <p>app</p>
+  return (
+    <div className="app">
+      <Router>
+        <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/login" component={Login} />
+              <Redirect to="/"/>
+            </Switch>
+          </div>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
