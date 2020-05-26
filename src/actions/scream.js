9f66f97f6f74b6e158619157bbb9bfff73a74d9a
DasH <fxfddfno@gmail.com>
2020-05-26 19:30:26 +0300

    get scream like and unlike actions

---
+++ src/actions/scream.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import {
+    SET_SCREAMS,
+    LOADING_SCREAMS,
+    LIKE_SCREAMS,
+    UNLIKE_SCREAMS
+} from "./types";
+
+
+export const getScreams = _ => async dispatch =>{
+    dispatch({ type: LOADING_SCREAMS });
+    try{
+        const res = await axios.get(`${process.env.REACT_APP_FUNCTION_URI}/screams`);
+        dispatch({
+            type: SET_SCREAMS,
+            payload: res.data
+        })
+    }catch(err){
+        dispatch({
+            type: SET_SCREAMS,
+            payload: []
+        })
+    }
+}
+
+export const likeScream = id => async dispatch =>{
+    try{
+        const res = await axios.patch(`${process.env.REACT_APP_FUNCTION_URI}/screams/${id}/like`);
+        dispatch({
+            type: LIKE_SCREAMS,
+            payload: res.data
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
+export const unLikeScream = id => async dispatch =>{
+    try{
+        const res = await axios.patch(`${process.env.REACT_APP_FUNCTION_URI}/screams/${id}/unlike`);
+        dispatch({
+            type: UNLIKE_SCREAMS,
+            payload: res.data
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -5,3 +5,9 @@ export const LOADING_UI = "LOADING_UI";
 export const SET_AUTH = "SET_AUTH";
 export const SET_UNAUTH = "SET_UNAUTH";
 export const LOADING_USER = 'LOADING_USER';
+
+// screams
+export const SET_SCREAMS = 'SET_SCREAMS';
+export const LOADING_SCREAMS = 'LOADING_SCREAMS'; 
+export const LIKE_SCREAMS = 'LIKE_SCREAMS'; 
+export const UNLIKE_SCREAMS = 'UNLIKE_SCREAMS'; 
